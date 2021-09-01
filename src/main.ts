438dfd5211ef58e80f5833416877b40d9ce37b71
Nam Pham <phamnam3@hotmail.com>
2021-08-31 20:45:37 -0400

    Made generic map, filter, and reduce functions using forEach

--- src/main.ts
+++ src/main.ts
@@ -42,3 +42,109 @@ console.log(
 console.log(
   `The remainder of ${firstOperand} divided by ${secondOperand} is ${remainder}`
 )
+
+function _map<TSource, TResult>(
+  sourceArray: Array<TSource>,
+  func: (element: TSource) => TResult
+): Array<TResult> {
+  const answer: Array<TResult> = []
+  sourceArray.forEach((element) => answer.push(func(element)))
+  return answer
+}
+
+const numbers = [1, 2, 3, 4, 5]
+const doubled = _map(numbers, function (number) {
+  return number * 2
+})
+
+const increased = _map(numbers, function (number) {
+  return number + 2
+})
+
+console.log(doubled)
+console.log(increased)
+
+const strings = ['one', 'two', 'three', 'four', 'five']
+const uppered = _map(strings, function (string) {
+  return string.toUpperCase()
+})
+
+console.log(uppered)
+
+// function _filter(
+//   numbers: number[],
+//   func: (number: number) => boolean
+// ): number[] {
+//   const answer = [0]
+//   numbers.forEach((number) => {
+//     if (func(number)) {
+//       answer.push(number)
+//     }
+//   })
+//   answer.splice(0, 1)
+//   return answer
+// }
+
+function _filter<TSource>(
+  sourceArray: TSource[],
+  func: (element: TSource) => boolean
+): TSource[] {
+  const answer: TSource[] = []
+  sourceArray.forEach((number) => {
+    if (func(number)) {
+      answer.push(number)
+    }
+  })
+  return answer
+}
+
+const numbersFiltered = _filter(numbers, function (number) {
+  return number > 3
+})
+
+console.log(numbersFiltered)
+
+const stringsFiltered = _filter(strings, function (string) {
+  return string === 'three'
+})
+
+console.log(stringsFiltered)
+
+// function _reduce(
+//   numbers: number[],
+//   func: (total: number, number: number) => number,
+//   initialValue: number
+// ): number {
+//   let answer = initialValue
+//   numbers.forEach((number) => (answer = func(answer, number)))
+//   return answer
+// }
+
+function _reduce<TSource, TReduced>(
+  numbers: TSource[],
+  func: (total: TReduced, number: TSource) => TReduced,
+  initialValue: TReduced
+): TReduced {
+  let answer = initialValue
+  numbers.forEach((number) => (answer = func(answer, number)))
+  return answer
+}
+
+const numbersReduced = _reduce(
+  numbers,
+  function (total, number) {
+    return total + number
+  },
+  0
+)
+
+const stringsTotalLetters = _reduce(
+  strings,
+  function (numberOfLetters, string) {
+    return numberOfLetters + string.length
+  },
+  0
+)
+
+console.log(numbersReduced)
+console.log(stringsTotalLetters)
